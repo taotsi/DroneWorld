127329c59a961857340d3845bf2b2ed4b589c7c6
taotsi <tmkf.guthrie@gmail.com>
2018-10-02 23:44:13 +0800

    plot pillars, tested

--- py_client/rpc_client.py
+++ py_client/rpc_client.py
@@ -1,13 +1,17 @@
 import msgpackrpc
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 client = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", 8080))
 
 pillars = client.call("GetPillarFrame")
-print(pillars)
 
-"""
-kde = client.call("GetKde")
-plt.plot(kde)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for i in range(len(pillars)):
+    ax.plot([pillars[i][0], pillars[i][0]],
+            [pillars[i][1], pillars[i][1]],
+            [pillars[i][2], pillars[i][3]])
+
 plt.show()
-"""
