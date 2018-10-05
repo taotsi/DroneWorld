e6e269268244461fcda59731429b61f38dca3eab
taotsi <tmkf.guthrie@gmail.com>
2018-10-05 23:42:59 +0800

    test

--- py_client/rpc_client.py
+++ py_client/rpc_client.py
@@ -4,14 +4,21 @@ from mpl_toolkits.mplot3d import Axes3D
 
 client = msgpackrpc.Client(msgpackrpc.Address("127.0.0.1", 8080))
 
+"""
 pillars = client.call("GetPillarFrame")
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-
 for i in range(len(pillars)):
     ax.plot([pillars[i][0], pillars[i][0]],
             [pillars[i][1], pillars[i][1]],
-            [pillars[i][2], pillars[i][3]])
-
+            [-pillars[i][2], -pillars[i][3]])
+plt.show()
+"""
+clusters = client.call("GetPillarCluster")
+fig = plt.figure()
+ax = fig.add_subplot(111, projection="3d")
+for cl in clusters:
+    for pl in cl:
+        ax.plot([pl[0], pl[0]], [pl[1], pl[1]], [-pl[2], -pl[3]])
 plt.show()
